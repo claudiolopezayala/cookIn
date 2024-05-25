1323c5d0ec0866c405773901d83245f171a0907d
Claudio <claudioalejandro4@gmail.com>
2024-05-25 15:26:30 -0600

    comment model

--- src/java/org/lasalle/services/model/Follow.java
+++ src/java/org/lasalle/services/model/Follow.java
@@ -9,5 +9,40 @@ package org.lasalle.services.model;
  * @author Claudio
  */
 public class Follow {
+    /*CREATE TABLE followers (
+  id integer PRIMARY KEY auto_increment,
+  accountFollowedId integer not null,
+  accountThatFollowsId integer not null
+);
+*/
+    private int id;
+    private User accountFollowed;
+    private User accountThatFollows;
+
+    public Follow(User accountFollowed, User accountThatFollows) {
+        this.accountFollowed = accountFollowed;
+        this.accountThatFollows = accountThatFollows;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User getAccountFollowed() {
+        return accountFollowed;
+    }
+
+    public void setAccountFollowed(User accountFollowed) {
+        this.accountFollowed = accountFollowed;
+    }
+
+    public User getAccountThatFollows() {
+        return accountThatFollows;
+    }
+
+    public void setAccountThatFollows(User accountThatFollows) {
+        this.accountThatFollows = accountThatFollows;
+    }
+    
     
 }
